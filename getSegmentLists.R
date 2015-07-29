1b0128836aabdc361b0fb5d22969196f42461b58
RaymondNelson <raymond.nelson@gmail.com>
2015-07-29 14:59:40 -0400

    working again

--- getSegmentLists.R
+++ getSegmentLists.R
@@ -7,10 +7,18 @@
 
 
 
-# library(stringr)
-# 
-# # get exam names from the _Data data frames
-# uniqueExams <- unique(str_sub(ls(pattern="*_Data$", pos=1),1, -6))
+library(stringr)
+
+# get exam names from the _Data data frames
+uniqueExams <- unique(str_sub(ls(pattern="*_Data$", pos=1),1, -6))
+
+
+cps <- 30
+prestimSeg <- 10
+EDALat <- .5
+CardioLat <- .5
+ROWEnd <- 5
+measuredSeg <- 15
 
 
 ###
@@ -28,7 +36,7 @@ getDataSegmentLists <- function(x=uniqueExams, makeList=TRUE) {
   ####
   
   # loop over the exams
-  # i <- 1
+  # i=1
   for (i in 1:length(uniqueExams)) {
     examName <- uniqueExams[i]
     
@@ -36,67 +44,70 @@ getDataSegmentLists <- function(x=uniqueExams, makeList=TRUE) {
     # uses glob2rx to make the regex searchstring
     eventLists <- ls(pattern=glob2rx(paste0(examName, "*_eventList$")), pos=1) 
     
-
-    
-#     # use the data frame of all exam data to get the names of unique series
-#     examData <- get(paste0(examName, "_Data"), pos=1)
-#     uniqueSeries <- unique(strtrim(examData$chartName, 1))
+    # use the data frame of all exam data to get the names of unique series
+    examDF <- get(paste0(examName, "_Data"), pos=1)
+    uniqueSeries <- unique(strtrim(examDF$chartName, 1))
 
 
 
     # get the list names for the data for each unique series
-    seriesNames <- ls(pattern=glob2rx(paste0(examName, "*_Charts")), pos=1)
+    # uniqueSeries <- ls(pattern=glob2rx(paste0("*", examName, "*_eventList*"), trim.head=TRUE, trim.tail=TRUE), pos=1)
     # trim the series names
-    # seriesNames <- str_sub(seriesNames, -8, -8)
+    # uniqueSeries <- str_sub(uniqueSeries, -8, -8)
     
     # loop over each unique series in each exam
-    # j <- 1
-    for (j in 1:length(seriesNames)) {
-      seriesName <- seriesNames[j]
+    # j=1
+    for (j in 1:length(uniqueSeries)) {
+      seriesName <- uniqueSeries[j]
       
 #       searchString <- paste0(seriesName, ".")
-#       uniqueCharts <- unique(grep(searchString, examData$chartName, value=TRUE))
+#       uniqueCharts <- unique(grep(searchString, examDF$chartName, value=TRUE))
       
-        # get the series data
-        seriesData <- get(seriesName, pos=1)
-        uniqueCharts <- names(seriesData)      
-        
+      # get the series data
+      # seriesDF <- get(seriesName, pos=1)
+      seriesDF <- examDF[examDF$seriesName==uniqueSeries[j],]
+
+      # uniqueCharts <- names(seriesDF)      
+      uniqueCharts <- as.character(unique(seriesDF$chartName))
+
       # loop over unique chart for each series for each exam
-      # k <- 1
+      # k=1
       for (k in 1:length(uniqueCharts)) {
         chartName <- uniqueCharts[k]
         
-        #chartData <- examData[examData$chartName==chartName,]      
-        chartData <- seriesData[[k]]
+        # chartDF <- seriesDF[[k]]
+        chartDF <- examDF[examDF$chartName==chartName,]      
         
         # make a variable to hold the results
         dataSegmentList <- NULL
         
         # get the eventList for the chart
-        myEventList <- get(paste0(examName, "_", chartName, "_eventList"), pos=1)
+        chartEventList <- get(paste0(examName, "_", chartName, "_eventList"), pos=1)
         
         # loop over the list of vectors
         # and save the result as a list of stimulus segments
         # for each unique chart
         # with the same name as the event labels
-        # l <- 1
-        for (l in 1:length(myEventList)) {
+        # l=1
+        for (l in 1:length(chartEventList)) {
           
-          startRow <- unlist(myEventList[l])[2]
-          # startRow <- myEventList[l,2]
-          ifelse(startRow>=5*30, 
-                 prestimRow<-startRow-5*30, 
+          startRow <- unlist(chartEventList[l])[2]
+          # startRow <- chartEventList[l,2]
+          ifelse(startRow>=prestimSeg*cps, 
+                 prestimRow<-startRow-prestimSeg*cps, 
                  prestimRow<-1)
-          endRow <- startRow + 25*30
-          mySegment <- chartData[prestimRow:endRow,]
+          endRow <- startRow + 25*cps
+          mySegment <- chartDF[prestimRow:endRow,]
           dataSegmentList[[l]] <- mySegment
         }
-        print(names(myEventList))
-        # names(dataSegmentList) <- myEventList$Label
-        names(dataSegmentList) <- names(myEventList)
-        dataSegmentListName <- paste0(examName, "_", chartName, "_dataSegmentList")
+        print(names(chartEventList))
+        # names(dataSegmentList) <- chartEventList$Label
+        
+        # add the names after the loop is complete
+        names(dataSegmentList) <- names(chartEventList)
         
         #save each eventList
+        dataSegmentListName <- paste0(examName, "_", chartName, "_dataSegmentList")
         if(makeList==TRUE) {
           assign(dataSegmentListName, dataSegmentList, pos=1)
         } # end if
