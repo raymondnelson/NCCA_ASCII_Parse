e98d654658fd2397cefc433b350fb713456bbc4f
RaymondNelson <raymond.nelson@gmail.com>
2015-07-29 13:46:01 -0400

    substantial changes to direct the process pneumo output to data frames instead of list ouput

--- pneumoSigProc.R
+++ pneumoSigProc.R
@@ -9,7 +9,7 @@ library(stringr)
 
 
 # get exam names from the _Data data frames
-# uniqueExams <- unique(str_sub(ls(pattern="*_Data$", pos=1),1, -6))
+uniqueExams <- unique(str_sub(ls(pattern="*_Data$", pos=1),1, -6))
 
 
 
@@ -19,7 +19,7 @@ library(stringr)
 
 pneumoSigProc <- function(x=uniqueExams, 
                         output=FALSE, 
-                        showNames=FALSE) {
+                        showNames=TRUE) {
   # function to apply a digital filter to the time series data
   # x input is a list of unique exams
   # the input data is the output from the function in the centerData.R script
@@ -43,60 +43,90 @@ pneumoSigProc <- function(x=uniqueExams,
   uniqueExams <- x
   
   # loop over each exam in the list 
-  # i <- 1
+  # i=1
   for(i in 1:length(uniqueExams)) {
     
     examName <- uniqueExams[i]
     
+    if(showNames==TRUE) print(examName)
+    
     # get the names of time series lists for all unique series in each exam
-    searchString <- paste0(examName, "*", "_Charts")
-    seriesNames <- ls(pattern=glob2rx(searchString), pos=1)
+    searchString <- paste0("*", examName, "_Data", "*")
+    # uniqueSeries <- ls(pattern=glob2rx(searchString, trim.head=TRUE, trim.tail=TRUE), pos=1)
+    # uniqueSeries <- ls(pattern=glob2rx(searchString), pos=1)
+    
+    examDF <- get(glob2rx(searchString, trim.head=TRUE, trim.tail=TRUE), pos=1)
+    
+    # add 2 new columns for the processed pneumoData
+    examDF$c_UPneumoS <- rep(0, times=nrow(examDF))
+    examDF$c_LPneumoS <- rep(0, times=nrow(examDF))
+    
+    # get the names of unique series
+    uniqueSeries <- as.character(unique(examDF$seriesName))
+    
+    # make an empty list to hold the output
+    outputList <- NULL
     
     # loop over each unique series
-    # j <- 1
-    for(j in 1:length(seriesNames)) {
+    # j=1
+    for(j in 1:length(uniqueSeries)) {
       
-      if(showNames==TRUE) print(seriesNames[j])
+      if(showNames==TRUE) print(paste("series", uniqueSeries[j]))
       
       # get the list of time series data for the charts in the exam
-      seriesData <- get(seriesNames[j], pos=1)
-      chartNames <- names(seriesData)
+      # seriesDF <- get(uniqueSeries[j], pos=1)
+      seriesDF <- examDF[examDF$seriesName==uniqueSeries[j],]
+      
+      # uniqueCharts <- names(seriesDF)
+      uniqueCharts <- as.character(unique(seriesDF$chartName))
       
       # loop over each chart in the series 
-      # k <- 1
-      for(k in 1:length(seriesData)) {
+      # k=1
+      for(k in 1:length(uniqueCharts)) {
         # get the data frame with the time series data for each chart in the series
-        chartData <- seriesData[[k]]
+        # chartDF <- seriesDF[[k]]
+        chartDF <- examDF[examDF$chartName==uniqueCharts[k],]
+        
+        if(showNames==TRUE) print(uniqueCharts[k])
+        
+        chartOnsetRow <- which(examDF$chartName==uniqueCharts[k])[1]
         
         # apply the filter functions to the data frame columns
         
-        chartData$UPneumoS <- lowPass.886(x=chartData$UPneumo)
-        chartData$LPneumoS <- lowPass.886(x=chartData$LPneumo)
+        chartDF$c_UPneumoS <- lowPass.886(x=chartDF$c_UPneumo)
+        chartDF$c_LPneumoS <- lowPass.886(x=chartDF$c_LPneumo)
         
-#         chartData$UPneumoMean <- tsMean(x=chartData$UPneumoS)
-#         chartData$UPneumoVar <- tsVar(x=chartData$UPneumoS)
+#         chartDF$UPneumoMean <- tsMean(x=chartDF$UPneumoS)
+#         chartDF$UPneumoVar <- tsVar(x=chartDF$UPneumoS)
 #         
-#         chartData$LPneumoMean <- tsMean(x=chartData$LPneumoS)
-#         chartData$LPneumoVar <- tsVar(x=chartData$LPneumoS)
+#         chartDF$LPneumoMean <- tsMean(x=chartDF$LPneumoS)
+#         chartDF$LPneumoVar <- tsVar(x=chartDF$LPneumoS)
         
         # plot.ts()
         
         # save the result to the list
-        seriesData[[k]] <- chartData
+        outputList[[k]] <- chartDF
         
-        if(showNames==TRUE) print(chartNames[k])
-        
+        # save the chartDF to the examDF
+        examDF[chartOnsetRow:(nrow(chartDF)+chartOnsetRow-1),] <- chartDF
+
       } # end for loop over each chart in each series
       
-      # save the list for the unique series
-      assign(seriesNames[j], seriesData, pos=1)
+      # name the data frames in the ouput list
+      names(outputList) <- uniqueCharts
+
+#       # save the list for the unique series
+#       assign(uniqueSeries[j], seriesDF, pos=1)
       
     } # end loop over unique series
-    
+
+    # save the examDF to the global environment with the centered data
+    assign(paste0(examName, "_Data"), examDF, pos=1)
+
   } # end loop over unique exams
   
   # return the last
-  if(output==TRUE) return(seriesData) 
+  if(output==TRUE) return(seriesDF) 
   
 } # end pneumoSigProc function
 
