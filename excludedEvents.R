c7d32744c12b79e84ee5f74cefd7c93a8038fdcc
Raymond Nelson <raymond.nelson@gmail.com>
2016-01-08 16:05:13 -0500

    events to exclude from measurement

---
+++ excludedEvents.R
@@ -0,0 +1,3 @@
+# excluded events
+# are not measured 
+excludeEvents <- c("BI", "SW", "X", "XX", "WRQ", "RS", "TI", "EI", "EE", "MV", "MVT", "MI", "CA", "AI", "TDB", "SLP", "WU", "CA", "OS", "OTH", "B", "T", "C", "Y", "BN", "SNF", "CT", "LGH", "DB", "OSN", "ISN")
